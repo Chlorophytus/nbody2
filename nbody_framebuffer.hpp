6a18f78f6f2b706d070ff38b634cb0f07f691276
Roland Metivier <metivier.roland@chlorophyt.us>
2019-08-06 00:19:55 -0500

    work on fb, add in init/deinit actual

--- nbody_framebuffer.hpp
+++ nbody_framebuffer.hpp
@@ -6,19 +6,10 @@
 namespace nbody {
     namespace framebuffer {
         /** A singleton for particle rendering
-         *  - pixels = the pixel data
-         *  - window, renderer, surface, texture = SDL2 data structures, INTERNAL
-         *
          *  - init(), deinit() : make and destroy the singleton
          *  - poll_and_tick() : if this returns true then the application should quit
          */
-        std::uint32_t *pixels;
-        SDL_Window *window;
-        SDL_Renderer *renderer;
-        SDL_Surface *surface;
-        SDL_Texture *texture;
-
-        void init(std::uint16_t, std::uint16_t, SDL_WindowFlags);
+        void init(std::uint16_t, std::uint16_t, int);
 
         bool poll_and_tick();
 
