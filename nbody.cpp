84271fde3faa303c79b4a03e5c5346f9fc49f3f9
Roland Metivier <metivier.roland@chlorophyt.us>
2019-08-12 14:16:39 -0500

    add res change, refactor loop

--- nbody.cpp
+++ nbody.cpp
@@ -3,16 +3,19 @@
 
 int main(int argc, const char *argv[]) {
     // Do logic. If we can't then assert and die.
-    auto particle_groups = 64;
+    auto particle_groups = 36;
     if(argc == 2)
         particle_groups = atoi(argv[1]);
     particle_groups *= 9;
     particle_groups *= std::thread::hardware_concurrency();
     std::fprintf(stdout, "NBody2 v0.1.0 by Roland Metivier\n");
     std::fprintf(stdout, "...with %d particles\n", particle_groups);
-    nbody::framebuffer::init(1600, 900, 0, particle_groups);
-    while (nbody::framebuffer::poll_and_tick())
-        ;
+    nbody::framebuffer::init(1920, 1080, 0, particle_groups);
+    auto run = true;
+    do {
+        run = nbody::framebuffer::poll_and_tick();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    } while (run);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     nbody::framebuffer::deinit();
